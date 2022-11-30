7710a8a3e5c5e755c4f227d355c251f873e509b9
Dominik Jaszkowski <75068588+dominikjaszkowski@users.noreply.github.com>
2022-11-30 17:30:51 +0100

    Update controllerUserData.php

--- controllerUserData.php
+++ controllerUserData.php
@@ -16,6 +16,7 @@ if(isset($_POST['signup'])){
     $lowercase = preg_match('@[a-z]@', $password);
     $number = preg_match('@[0-9]@', $password);
     $specialChars = preg_match('@[^\w]@', $password);
+    
     if (!preg_match("/^[a-zA-Z -]+$/",$name)) {
         $errors['name'] = "Imie musi się składać tylko z liter!";
       } 
@@ -35,9 +36,90 @@ if(isset($_POST['signup'])){
     }
     if(count($errors) === 0){
         $encpass = password_hash($password, PASSWORD_BCRYPT);
-        $insert_data = "INSERT INTO user (imie, nazwisko, email, haslo)
-                        values('$name', '$surname', '$email', '$encpass')";
+        $code = rand(999999, 111111);
+        $status = "notverified";
+        $insert_data = "INSERT INTO user (imie, nazwisko, email, haslo, kod, status)
+                        values('$name', '$surname', '$email', '$encpass', '$code', '$status')";
         $data_check = mysqli_query($con, $insert_data);
+        if($data_check){
+            $subject = "Kod weryfikacyjny email";
+            $message = "Twój kod weryfikacyjny to $code";
+            $sender = "From: kalat123456789@gmail.com";
+            if(mail($email, $subject, $message, $sender)){
+                $info = "Wysłaliśmy twój kod weryfikacyjny na email - $email";
+                $_SESSION['info'] = $info;
+                $_SESSION['email'] = $email;
+                $_SESSION['password'] = $password;
+                header('location: user-otp.php');
+                exit();
+            }else{
+                $errors['otp-error'] = "Nie udało się wysłać kodu!";
+            }
+        }else{
+            $errors['db-error'] = "Nie powiodło się podczas wstawiania danych do bazy!";
+        }
     }
-} 
+
+}
+
+    //Po wciśnięciu przycisku weryfikacji
+    if(isset($_POST['check'])){
+        $_SESSION['info'] = "";
+        $otp_code = mysqli_real_escape_string($con, $_POST['otp']);
+        $check_code = "SELECT * FROM user WHERE kod = $otp_code";
+        $code_res = mysqli_query($con, $check_code);
+        if(mysqli_num_rows($code_res) > 0){
+            $fetch_data = mysqli_fetch_assoc($code_res);
+            $fetch_code = $fetch_data['kod'];
+            $email = $fetch_data['email'];
+            $code = 0;
+            $status = 'verified';
+            $update_otp = "UPDATE user SET kod = $code, status = '$status' WHERE kod = $fetch_code";
+            $update_res = mysqli_query($con, $update_otp);
+            if($update_res){
+                $_SESSION['imie'] = $name;
+                $_SESSION['email'] = $email;
+                header('location: home.php');
+                exit();
+            }else{
+                $errors['otp-error'] = "Nie powiodło się podczas aktualizacji kodu!";
+            }
+        }else{
+            $errors['otp-error'] = "Wprowadziłeś nieprawidłowy kod!";
+        }
+    }
+
+    //Po wciśnięciu przycisku login
+    if(isset($_POST['login'])){
+        $email = mysqli_real_escape_string($con, $_POST['email']);
+        $password = mysqli_real_escape_string($con, $_POST['password']);
+        $check_email = "SELECT * FROM user WHERE email = '$email'";
+        $res = mysqli_query($con, $check_email);
+        if(mysqli_num_rows($res) > 0){
+            $fetch = mysqli_fetch_assoc($res);
+            $fetch_pass = $fetch['haslo'];
+            if(password_verify($password, $fetch_pass)){
+                $_SESSION['email'] = $email;
+                $status = $fetch['status'];
+                if($status == 'verified'){
+                  $_SESSION['email'] = $email;
+                  $_SESSION['haslo'] = $password;
+                    header('location: home.php');
+                }else{
+                    $info = "Wygląda na to, że nie zweryfikowałeś jeszcze swojego maila. - $email";
+                    $_SESSION['info'] = $info;
+                    header('location: user-otp.php');
+                }
+            }else{
+                $errors['email'] = "Nieprawidłowy email lub hasło!";
+            }
+        }else{
+            $errors['email'] = "Wygląda na to, że nie jesteś jeszcze zarejestrowany! Kliknij na dolny link, aby się zarejestrować.";
+        }
+    }
+
+    if(isset($_POST['login-now'])){
+        header('Location: login-user.php');
+    }    
+
 ?>
