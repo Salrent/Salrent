797d930fab18269e73d53c123bd532b23ad6b5b3
Dominik Jaszkowski <75068588+dominikjaszkowski@users.noreply.github.com>
2022-12-07 17:42:29 +0100

    SAL-7 Zmiana hasła

---
+++ forgot-password.php
@@ -0,0 +1,42 @@
+<?php require_once "controllerUserData.php"; ?>
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <title>Zapomniane haslo</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+                <form action="forgot-password.php" method="POST" autocomplete="">
+                    <h2 class="text-center">Zapomniane haslo</h2>
+                    <p class="text-center">Wprowadz swoj adres email</p>
+                    <?php
+                        if(count($errors) > 0){
+                            ?>
+                            <div class="alert alert-danger text-center">
+                                <?php 
+                                    foreach($errors as $error){
+                                        echo $error;
+                                    }
+                                ?>
+                            </div>
+                            <?php
+                        }
+                    ?>
+                    <div class="form-group">
+                        <input class="form-control" type="email" name="email" placeholder="Wprowadź adres email" required value="<?php echo $email ?>">
+                    </div>
+                    <div class="form-group">
+                        <input class="form-control button" type="submit" name="check-email" value="Kontynuuj">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    
+</body>
+</html>
