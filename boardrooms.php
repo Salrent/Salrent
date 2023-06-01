7d4e9f4e4d3f66f234cb432c53e855d6daa78315
Dominik Jaszkowski <75068588+dominikjaszkowski@users.noreply.github.com>
2023-06-01 21:42:50 +0200

    SAL-26 boardrooms

---
+++ boardrooms.php
@@ -0,0 +1,470 @@
+<?php require_once "pages/controllerUserData.php"; ?>
+<?php require "pages/count_users.php"; ?>
+<?php
+$email = $_SESSION['email'];
+$password = $_SESSION['haslo'];
+if ($email != false && $password != false) {
+  $sql = "SELECT * FROM user WHERE email = '$email'";
+  $run_Sql = mysqli_query($con, $sql);
+  if ($run_Sql) {
+    $fetch_info = mysqli_fetch_assoc($run_Sql);
+    $status = $fetch_info['status'];
+    $code = $fetch_info['kod'];
+    $emaill = $fetch_info['email'];
+    if ($emaill == null) {
+      header('Location: pages/login.php');
+    } else if ($status == "verified") {
+      if ($code != 0) {
+        header('Location: pages/login.php');
+      }
+    } else {
+      header('Location: pages/login.php');
+    }
+  }
+} else {
+  header('Location: pages/login.php');
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Admin | Dashboard</title>
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Tempusdominus Bootstrap 4 -->
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <!-- iCheck -->
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- JQVMap -->
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- overlayScrollbars -->
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <!-- Daterange picker -->
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <!-- summernote -->
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="wrapper">
+
+  <!-- Preloader -->
+  <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="dist/img/SalrentLogo1.png" alt="SalrentLogo1" height="60" width="60">
+  </div>
+
+  <!-- Navbar -->
+  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="index.php" class="nav-link">Home</a>
+      </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">Contact</a>
+      </li>
+    </ul>
+
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+      <!-- Navbar Search -->
+      <li class="nav-item">
+        <a class="nav-link" data-widget="navbar-search" href="#" role="button">
+          <i class="fas fa-search"></i>
+        </a>
+        <div class="navbar-search-block">
+          <form class="form-inline">
+            <div class="input-group input-group-sm">
+              <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
+              <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                  <i class="fas fa-search"></i>
+                </button>
+                <button class="btn btn-navbar" type="button" data-widget="navbar-search">
+                  <i class="fas fa-times"></i>
+                </button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </li>
+
+      <!-- Messages Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-comments"></i>
+          <span class="badge badge-danger navbar-badge">3</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Brad Diesel
+                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">Call me whenever you can...</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  John Pierce
+                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">I got your message bro</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <!-- Message Start -->
+            <div class="media">
+              <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
+              <div class="media-body">
+                <h3 class="dropdown-item-title">
+                  Nora Silvester
+                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
+                </h3>
+                <p class="text-sm">The subject goes here</p>
+                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
+              </div>
+            </div>
+            <!-- Message End -->
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
+        </div>
+      </li>
+      <!-- Notifications Dropdown Menu -->
+      <li class="nav-item dropdown">
+        <a class="nav-link" data-toggle="dropdown" href="#">
+          <i class="far fa-bell"></i>
+          <span class="badge badge-warning navbar-badge">15</span>
+        </a>
+        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+          <span class="dropdown-item dropdown-header">15 Notifications</span>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-envelope mr-2"></i> 4 new messages
+            <span class="float-right text-muted text-sm">3 mins</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-users mr-2"></i> 8 friend requests
+            <span class="float-right text-muted text-sm">12 hours</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item">
+            <i class="fas fa-file mr-2"></i> 3 new reports
+            <span class="float-right text-muted text-sm">2 days</span>
+          </a>
+          <div class="dropdown-divider"></div>
+          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+          <i class="fas fa-expand-arrows-alt"></i>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
+          <i class="fas fa-th-large"></i>
+        </a>
+      </li>
+    </ul>
+  </nav>
+  <!-- /.navbar -->
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index.php" class="brand-link">
+      <img src="dist/img/SalrentLogo2.png" alt="SalrentLogo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">Admin Panel</span>
+    </a>
+
+    <!-- Sidebar -->
+    <div class="sidebar">
+      <!-- Sidebar user panel (optional) -->
+      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+        <div class="image">
+          <img src="dist/img/brek.png" class="img-circle elevation-2" alt="User Image">
+        </div>
+        <div class="info">
+          <a href="index.php" class="d-block"><?php echo $fetch_info['imie'] ?> <?php echo $fetch_info['nazwisko'] ?></a>
+        </div>
+      </div>
+
+      <!-- SidebarSearch Form -->
+      <div class="form-inline">
+        <div class="input-group" data-widget="sidebar-search">
+          <input class="form-control form-control-sidebar" type="search" placeholder="Search" aria-label="Search">
+          <div class="input-group-append">
+            <button class="btn btn-sidebar">
+              <i class="fas fa-search fa-fw"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <!-- Sidebar Menu -->
+      <nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          <li class="nav-item">
+            <a href="index.php" class="nav-link">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Dashboard
+              </p>
+            </a>
+          </li>
+          <li class="nav-header">EXAMPLES</li>
+          <li class="nav-item">
+            <a href="./index.php" class="nav-link">
+              <i class="nav-icon far fa-calendar-alt"></i>
+              <p>
+                Calendar
+                <span class="badge badge-info right">2</span>
+              </p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="./index.php" class="nav-link">
+              <i class="nav-icon far fa-image"></i>
+              <p>
+                Gallery
+              </p>
+            </a>
+          </li>
+                    <li class="nav-item">
+            <a href="boardrooms.php" class="nav-link active">
+              <i class="nav-icon fas fa-bed"></i>
+              <p>
+                Boardrooms
+              </p>
+            </a>
+          </li>
+          <li class="nav-header">MISCELLANEOUS</li>
+          <li class="nav-item">
+            <a href="pages/logout-user.php" class="nav-link">
+              <i class="nav-icon fas fa-sign-out-alt"></i>
+              <p>
+                Logout
+              </p>
+            </a>
+          </li>
+      </nav>
+      <!-- /.sidebar-menu -->
+    </div>
+    <!-- /.sidebar -->
+  </aside>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Boardrooms</h1>
+          </div><!-- /.col -->
+          <div class="col-sm-6">
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+  <!-- Content Wrapper. Contains page content -->
+  <section class="content">
+        <!-- Main row -->
+        <?php
+// Połączenie z bazą danych
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "salrent";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Sprawdzenie połączenia
+if ($conn->connect_error) {
+    die("Błąd połączenia: " . $conn->connect_error);
+}
+
+// Pobranie danych z bazy
+$sql = "SELECT * FROM sale";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // Wyświetlanie danych w tabeli z użyciem Bootstrapa
+    echo '<table class="table table-striped">';
+    echo '<thead><tr><th>nr</th><th>pietro</th><th>typ</th><th>miejsca</th><th>status</th></tr></thead>';
+    echo '<tbody>';
+
+    while ($row = $result->fetch_assoc()) {
+        echo '<tr><td>' . $row["nr"] . '</td><td>' . $row["pietro"] . '</td><td>' . $row["typ"] . '</td><td>' . $row["miejsca"] . '</td><td>';
+
+        if ($row["status"] == 1) {
+            echo '<span class="text-success fw-bold">Dostępna</span>';
+        } else {
+            echo '<span class="text-danger fw-bold">Niedostępna</span>';
+        }
+
+        echo '</td></tr>';
+    }
+
+    echo '</tbody>';
+    echo '</table>';
+
+    // Formularz do zmiany wartości
+    echo '<form method="post" action="">
+        <div class="form-group">
+            <label for="selected_room">Wybierz salę:</label>
+            <select class="form-control" name="selected_room">
+                <option value="" disabled selected>Wybierz salę</option>';
+
+    // Pobranie listy sal do wyboru
+    $rooms_sql = "SELECT nr FROM sale";
+    $rooms_result = $conn->query($rooms_sql);
+    while ($room_row = $rooms_result->fetch_assoc()) {
+        echo '<option value="' . $room_row["nr"] . '">' . $room_row["nr"] . '</option>';
+    }
+
+    echo '</select>
+        </div>
+        <div class="form-group">
+            <label for="new_value">Nowy status sali (0(niedostępny) lub 1(dostępny)):</label>
+            <input type="number" class="form-control" name="new_value">
+        </div>
+        <button type="submit" class="btn btn-primary">Zmiana dostępności</button>
+    </form>';
+
+    // Obsługa zmiany wartości
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $errors = array(); // Tablica na błędy
+
+        if (isset($_POST["selected_room"]) && !empty($_POST["selected_room"])) {
+            $selected_room = $_POST["selected_room"];
+            $new_value = $_POST["new_value"];
+
+            // Walidacja nowej wartości
+            if ($new_value === "") {
+                $errors[1] = 'Wprowadź poprawną wartość.';
+            } else if ($new_value != "0" && $new_value != "1") {
+                $errors[2] = 'Podano nieprawidłową wartość. Wprowadź 0 lub 1.';
+            } else {
+                // Aktualizacja wartości w bazie danych dla wybranej sali
+                $update_sql = "UPDATE sale SET status = '$new_value' WHERE nr = '$selected_room'";
+                if ($conn->query($update_sql) === TRUE) {
+                    echo '<div class="alert alert-success" role="alert">Wartość została zaktualizowana pomyślnie.</div>';
+                    echo '<meta http-equiv="refresh" content="0">';
+                } else {
+                    $errors[3] = 'Błąd podczas aktualizacji wartości: ' . $conn->error;
+                }
+            }
+        } else {
+            $errors[4] = 'Proszę wybrać poprawną sale oraz wartość.';
+        }
+        
+
+        // Wyświetlanie wszystkich błędów
+        if (!empty($errors)) {
+            echo '<div class="alert alert-danger" role="alert">';
+            echo '<ul>';
+            foreach ($errors as $error) {
+                echo '<li>' . $error . '</li>';
+            }
+            echo '</ul>';
+            echo '</div>';
+        }
+    }
+} else {
+    echo '<div class="alert alert-warning" role="alert">Brak danych w tabeli.</div>';
+}
+
+$conn->close();
+?>
+
+
+
+
+        <!-- /.row (main row) -->
+  </div><!-- /.container-fluid -->
+    <!-- /.content -->
+    <br><Br><br><br><Br><br>
+  </section>
+  <!-- /.content-wrapper -->
+  <footer class="main-footer">
+    <strong>Copyright &copy; 2022-2023 <a href="">Salrent</a>.</strong>
+    All rights reserved.
+  </footer>
+
+  <!-- Control Sidebar -->
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
+  <!-- /.control-sidebar -->
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- jQuery UI 1.11.4 -->
+<script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+<script>
+  $.widget.bridge('uibutton', $.ui.button)
+</script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- ChartJS -->
+<script src="plugins/chart.js/Chart.min.js"></script>
+<!-- Sparkline -->
+<script src="plugins/sparklines/sparkline.js"></script>
+<!-- JQVMap -->
+<script src="plugins/jqvmap/jquery.vmap.min.js"></script>
+<script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+<!-- jQuery Knob Chart -->
+<script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+<!-- daterangepicker -->
+<script src="plugins/moment/moment.min.js"></script>
+<script src="plugins/daterangepicker/daterangepicker.js"></script>
+<!-- Tempusdominus Bootstrap 4 -->
+<script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<!-- Summernote -->
+<script src="plugins/summernote/summernote-bs4.min.js"></script>
+<!-- overlayScrollbars -->
+<script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+<script src="dist/js/pages/dashboard.js"></script>
+</body>
+</html>
